1cfc837305f2206346a866ead871897ec3ecd18c
Ievgen <zhe.tovtin@gmail.com>
2024-01-09 11:55:32 -0600

    updated switch case

--- Day 2/2_conditional-statement-switch.js
+++ Day 2/2_conditional-statement-switch.js
@@ -2,6 +2,8 @@ function getLetter(s) {
   let letter;
   // Write your code here
 
+  console.log('s.charAt(0) => ', s.charAt(0));
+
   switch(s.charAt(0)) {
     case ( 'a' || 'e' || 'o' || 'i' || 'u'):
       letter = 'A';
@@ -14,9 +16,17 @@ function getLetter(s) {
       break;
     case ('z' || 'n' || 'p' || 'q' || 'r' || 's' || 't' || 'v' || 'w' || 'x' || 'y' ):
       letter = 'D';
+      break;
+    default:
+      console.log('ERROR: Wrong input');
   }
+
+  console.log('letter =>', letter);
   return letter;
 };
 
+getLetter('z');
+
 // String.prototype.charAt()
+// charAt() always indexes the string as a sequence of UTF-16 code units, so it may return lone surrogates. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
