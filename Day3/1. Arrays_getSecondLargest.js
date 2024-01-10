7f92764957e24313382e03b2774047b8b5e8f0f2
Ievgen <zhe.tovtin@gmail.com>
2024-01-10 12:18:58 -0600

    added first method of getSecondLargest

--- Day3/1. Arrays_getSecondLargest.js
+++ Day3/1. Arrays_getSecondLargest.js
@@ -1,5 +1,20 @@
+// first method
 function getSecondLargest(nums) {
-  // Complete the function
+    // Complete the function
+    let firstLargestNumber = 0;
+    let secondLargestNumber = 0;
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > firstLargestNumber) {
+            secondLargestNumber = firstLargestNumber;
+            firstLargestNumber = nums[i];
+        } else if(nums[i] < firstLargestNumber && nums[i] > secondLargestNumber) {
+            secondLargestNumber = nums[i];
+        }
+    }
+    
+    console.log(' secondLargestNumber => ', secondLargestNumber);
+    return secondLargestNumber;
 }
 
 getSecondLargest([2,3,6,6,5]);
