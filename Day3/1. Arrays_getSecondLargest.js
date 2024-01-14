412c4992cb1ae8291882f1293269c3ba9b19eb4d
Ievgen <zhe.tovtin@gmail.com>
2024-01-14 10:27:18 -0600

    updated function

--- Day3/1. Arrays_getSecondLargest.js
+++ Day3/1. Arrays_getSecondLargest.js
@@ -1,5 +1,5 @@
 // first method
-function getSecondLargest(nums) {
+/* function getSecondLargest(nums) {
     // Complete the function
     let firstLargestNumber = 0;
     let secondLargestNumber = 0;
@@ -12,9 +12,92 @@ function getSecondLargest(nums) {
             secondLargestNumber = nums[i];
         }
     }
-    
     console.log(' secondLargestNumber => ', secondLargestNumber);
     return secondLargestNumber;
 }
 
 getSecondLargest([2,3,6,6,5]);
+
+//........................... 2
+firstLargestNumber = 0;
+secondLargestNumber = 0;
+if(2 > 0) {
+  secondLargestNumber = firstLargestNumber = 0;
+  firstLargestNumber = 2;
+}
+//........................... 3
+firstLargestNumber = 2;
+secondLargestNumber = 0;
+
+if(3 > 2) {
+  secondLargestNumber = firstLargestNumber = 2;
+  firstLargestNumber = 3;
+}
+
+//........................... 6
+firstLargestNumber = 3;
+secondLargestNumber = 2;
+if(6 > 3) {
+  secondLargestNumber = firstLargestNumber = 3;
+  firstLargestNumber = 6;
+}
+
+//........................... 6
+firstLargestNumber = 6;
+secondLargestNumber = 3;
+
+if(6 > 6) {
+  // no actions
+} else if(6 < 6 && 6 > 3) {
+    // no actions
+}
+
+//........................... 5
+
+if(nums[i] > firstLargestNumber) {
+            secondLargestNumber = firstLargestNumber;
+            firstLargestNumber = nums[i];
+        } else if(nums[i] < firstLargestNumber && nums[i] > secondLargestNumber) {
+            secondLargestNumber = nums[i];
+        }
+
+firstLargestNumber = 6;
+secondLargestNumber = 3;
+
+if(5 > 6) {
+    // no actions
+} else if (5 < 6 && 5 > 3) {
+    secondLargestNumber = 5;
+} */
+
+/* function findSecondLargest(arr) { 
+    let firstLargestNumber;
+    let secondLargestNumber; 
+  
+    // There should be at least two elements 
+    if (arr.length < 2) { 
+        return "There not enough elements in array for this function"; 
+    } 
+  
+    // Sort the array in ascending order 
+    arr.sort(); 
+  
+    // Start from the second last element as  
+    // the largest element is at last 
+    for (let i = arr.length - 2; i >= 0; i--) { 
+        // If the element is not equal to the  
+        // largest element 
+        if (arr[i] !== arr[arr.length - 1]) { 
+            return "The second largest element is " + arr[i]; 
+        } 
+    } 
+  
+    return "There is no second largest element"; 
+} 
+  
+// Driver program to test the function 
+const arr = [12, 35, 10, 35, 10, 34, 1]; 
+  
+// Output: The second largest element is 34 
+console.log(findSecondLargest(arr)); */
+
