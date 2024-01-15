6eaba2f893ccc2ddf399f43da1db4b69764662c9
Ievgen <zhe.tovtin@gmail.com>
2024-01-15 13:58:46 -0600

    added count Objects function

---
+++ Day_4/2_count_objects.js
@@ -0,0 +1,18 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+
+function getCount(objects) {
+  let totalCountOfObjects = 0;
+  
+  for(const i in objects) {
+      if(objects[i].x === objects[i].y) {
+          totalCountOfObjects++;
+      }
+  }
+  
+  return totalCountOfObjects;
+}
