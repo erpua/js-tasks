216df6130b827801f2cf312c35775cfc6987f885
Ievgen <zhe.tovtin@gmail.com>
2024-01-15 13:48:45 -0600

    added second way of fucntion

--- Day_4/1_create_rectangle_object.js
+++ Day_4/1_create_rectangle_object.js
@@ -1,7 +1,7 @@
 /*
  * Complete the Rectangle function
  */
-function Rectangle(a, b) {
+/* function Rectangle(a, b) {
     
   const rectangleObject = {
       length: a,
@@ -11,5 +11,13 @@ function Rectangle(a, b) {
   };
   
   return rectangleObject;
+}; */
+
+function Rectangle(a, b) {
+  this.length = a;
+  this.width = b;
+  this.perimeter = 2*(a+b);
+  this.area = a*b;
+  Object.freeze(this);
 };
 
