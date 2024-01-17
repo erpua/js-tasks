d0f3fb4f04923a29be15e171b9e97431c76cef15
Ievgen <zhe.tovtin@gmail.com>
2024-01-17 06:51:55 -0600

    added inheritance

--- Day_4/3_classes.js
+++ Day_4/3_classes.js
@@ -26,14 +26,22 @@ class Polygon {
     }, 0 );
   } */
 
-  perimeter() { 
+ /*  perimeter() { 
     let polygonPerimeter = this.polygonSides.reduce((accumulator, currentValue) => 
         accumulator + currentValue,
        0,  
     );
     return polygonPerimeter;
-  };
+  }; */
 
+};
+
+Polygon.prototype.perimeter = function() { 
+  let polygonPerimeter = this.polygonSides.reduce((accumulator, currentValue) => 
+      accumulator + currentValue,
+     0,  
+  );
+  return polygonPerimeter;
 };
 
 const rectangle = new Polygon([10, 20, 10, 20]);
@@ -45,3 +53,23 @@ console.log(square.perimeter());
 console.log(pentagon.perimeter());
 
 // https://docs.github.com/en/enterprise-server@3.8/get-started/using-git/pushing-commits-to-a-remote-repository
+// The git push command takes two arguments:
+
+/* 
+
+A remote name, for example, origin
+A branch name, for example, main
+For example:
+
+git push REMOTE-NAME BRANCH-NAME
+As  an example, you usually run git push origin main to push your local changes to your online repository.
+
+*/
+
+// Loose equality
+console.log(1 == '1');
+// true
+
+// Strict equality
+console.log(1 === '1');
+// false
